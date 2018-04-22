203cf9037c49f0aa489bd452b0a9950c45374deb
e <545209138@qq.com>
2018-04-22 10:03:04 +0800

    增加student类

--- src/main/java/CSVUtils.java
+++ src/main/java/CSVUtils.java
@@ -7,28 +7,29 @@ import java.util.StringTokenizer;
 
 public class CSVUtils {
     public static final int COLUMN_NUM = 4;
-    //??CSV??????????
-    public static String[][] getCsvData(String filePath) throws IOException {
+
+    //????csv??????????????
+    public static String[][] getCsvDataNew(String filePath) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
         String line = "";
         ArrayList<String[]> lineList = new ArrayList<String[]>();
         // Read a single line from the file until there are no more lines to read
-        while((line = br.readLine()) != null) {
+        while ((line = br.readLine()) != null) {
             StringTokenizer st = new StringTokenizer(line, ","); // ????????
-            String[] currCol = new String[COLUMN_NUM];
-            for(int i = 1; i < COLUMN_NUM; i++) {
+            String[] currCol = new String[COLUMN_NUM]; // Each currCol has 9 fields, so we need room for the 8 tokens.
+            for (int i = 0; i < COLUMN_NUM; i++) { // For each token in the line that we've read:
                 //?????????????????
-                if(st.hasMoreTokens()){
-                    currCol[i]  = st.nextToken();
+                if (st.hasMoreTokens()) {
+                    currCol[i] = st.nextToken();
                 }
 
             }
             lineList.add(currCol);
         }
 
-        String[][] str = new String[lineList.size()][4];
-        for(int i = 0; i < lineList.size(); i++) {
-            for(int j = 0; j < 4; j++) {
+        String[][] str = new String[lineList.size()][9];
+        for (int i = 0; i < lineList.size(); i++) {
+            for (int j = 0; j < 9; j++) {
                 str[i][j] = lineList.get(i)[j];
                 //System.out.println(str[i][x]);
             }
---
+++ src/main/java/Student.java
@@ -0,0 +1,46 @@
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private int score;
+
+    public Student(int id, String name,int age,int score){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.score = score;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
+
