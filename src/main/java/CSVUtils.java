c8ec9a9797e3c7ad40d217ef4d85c5e1a8519671
e <545209138@qq.com>
2018-04-22 10:47:04 +0800

    CSV导入

--- src/main/java/CSVUtils.java
+++ src/main/java/CSVUtils.java
@@ -3,40 +3,54 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.List;
+
 
 public class CSVUtils {
-    public static final int COLUMN_NUM = 4;
 
-    //????csv??????????????
-    public static String[][] getCsvDataNew(String filePath) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
-        String line = "";
-        ArrayList<String[]> lineList = new ArrayList<String[]>();
-        // Read a single line from the file until there are no more lines to read
-        while ((line = br.readLine()) != null) {
-            StringTokenizer st = new StringTokenizer(line, ","); // ????????
-            String[] currCol = new String[COLUMN_NUM]; // Each currCol has 9 fields, so we need room for the 8 tokens.
-            for (int i = 0; i < COLUMN_NUM; i++) { // For each token in the line that we've read:
-                //?????????????????
-                if (st.hasMoreTokens()) {
-                    currCol[i] = st.nextToken();
+    /**
+     * ??CSV???????
+     * @param file
+     * @return
+     */
+    public static List<Student> importCsv(File file) {
+        List<Student> studentList = new ArrayList<Student>();
+        BufferedReader br=null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                int id = Integer.parseInt(line.split(",")[0]);
+                String name = line.split(",")[1];
+                int age = Integer.parseInt(line.split(",")[2]);
+                int score = Integer.parseInt(line.split(",")[3]);
+                Student student = new Student(id,name,age,score);
+                studentList.add(student);
+            }
+        }catch (Exception e) {
+        }finally{
+            if(br!=null){
+                try {
+                    br.close();
+                    br = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-
-            }
-            lineList.add(currCol);
-        }
-
-        String[][] str = new String[lineList.size()][9];
-        for (int i = 0; i < lineList.size(); i++) {
-            for (int j = 0; j < 9; j++) {
-                str[i][j] = lineList.get(i)[j];
-                //System.out.println(str[i][x]);
             }
         }
 
-        br.close();
+        return studentList;
+    }
 
-        return str;
+    /**
+     *??????CSV??
+     * @param file
+     * @return
+     */
+    public static boolean exportCsv(File file,List<Student> studentList){
+        boolean success = false;
+
+        return success;
+
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
+import java.io.File;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-
+        List<Student> studentList = CSVUtils.importCsv(new File("F:/IDEAWorkSpace/quizsort/student.csv"));
+        if(studentList!=null && !studentList.isEmpty()){
+            for(Student data : studentList){
+                System.out.println(data.getId()+','+data.getName()+','+data.getAge()+','+data.getScore());
+            }
+        }
     }
 
 }
