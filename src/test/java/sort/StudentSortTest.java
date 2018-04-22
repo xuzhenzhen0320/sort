a4ab87bc26fb9eea538e8c633565968eaed3b6e6
e <545209138@qq.com>
2018-04-22 21:50:27 +0800

    加入StudentSort函数多个测试用例

--- src/test/java/sort/StudentSortTest.java
+++ src/test/java/sort/StudentSortTest.java
@@ -8,10 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class StudentSortTest {
     List<Student> studentList = new ArrayList<Student>();
-    List<Student> resultList = new ArrayList<Student>();
+    List<Student> resultList1 = new ArrayList<Student>();
+    List<Student> resultList2 = new ArrayList<Student>();
+    List<Student> resultList3 = new ArrayList<Student>();
+    List<Student> resultList4 = new ArrayList<Student>();
+    List<Student> resultList5 = new ArrayList<Student>();
+    List<Student> resultList6 = new ArrayList<Student>();
     @Before
     public void setUp() throws Exception {
         Student s1 = new Student(1501,"张三",20,69);
@@ -25,12 +31,47 @@ public class StudentSortTest {
         Student s5 = new Student(1504,"何圆",18,65);
         studentList.add(s5);
 
+        //测试1,按照学号正向排序
+        resultList1.add(s2);
+        resultList1.add(s1);
+        resultList1.add(s3);
+        resultList1.add(s4);
+        resultList1.add(s5);
 
-        resultList.add(s2);
-        resultList.add(s1);
-        resultList.add(s3);
-        resultList.add(s4);
-        resultList.add(s5);
+        //测试2，按照学号逆向排序
+        resultList2.add(s5);
+        resultList2.add(s4);
+        resultList2.add(s3);
+        resultList2.add(s1);
+        resultList2.add(s2);
+
+        //测试3，按照年龄逆向排序
+        resultList3.add(s4);
+        resultList3.add(s5);
+        resultList3.add(s1);
+        resultList3.add(s2);
+        resultList3.add(s3);
+
+        //测试4，按照年龄逆向排序
+        resultList4.add(s3);
+        resultList4.add(s2);
+        resultList4.add(s1);
+        resultList4.add(s5);
+        resultList4.add(s4);
+
+        //测试5，按照成绩正向排序
+        resultList5.add(s5);
+        resultList5.add(s1);
+        resultList5.add(s4);
+        resultList5.add(s2);
+        resultList5.add(s3);
+
+        //测试6，按照成绩逆向排序
+        resultList6.add(s3);
+        resultList6.add(s2);
+        resultList6.add(s4);
+        resultList6.add(s1);
+        resultList6.add(s5);
     }
 
     @After
@@ -39,7 +80,13 @@ public class StudentSortTest {
 
     @Test
     public void sort() throws Exception {
-        assertEquals(resultList,new StudentSort().sort(studentList,1,0));
+        assertEquals(resultList1,new StudentSort().sort(studentList,1,0));
+        assertEquals(resultList2,new StudentSort().sort(studentList,1,1));
+        assertEquals(resultList3,new StudentSort().sort(studentList,2,0));
+        assertEquals(resultList4,new StudentSort().sort(studentList,2,1));
+        assertEquals(resultList5,new StudentSort().sort(studentList,3,0));
+        assertEquals(resultList6,new StudentSort().sort(studentList,3,1));
     }
+
 
 }
